a7b05b867a4029fa6e0e7142155abccd41a77f4c
Nikolas Wise <nikolaswise@gmail.com>
2015-03-27 09:05:58 -0700

    flesh out

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,21 @@
-console.log('hey');
+console.log('well here goes nothing ...')
+
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+  host: 'http://52.0.223.139:9200',
+  log: 'trace'
+});
+
+client.ping({
+  requestTimeout: 30000,
+
+  // undocumented params are appended to the query string
+  hello: "elasticsearch!"
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
